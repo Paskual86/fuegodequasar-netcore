2e4a220a371526ba4737210cc07b3f65d0db05a5
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-03-08 22:43:42 -0300

    Add test service

---
+++ FuegoDeQuasar/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuegoDeQuasar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($" Fuego de Quasar API is running...");
+        }
+    }
+}
