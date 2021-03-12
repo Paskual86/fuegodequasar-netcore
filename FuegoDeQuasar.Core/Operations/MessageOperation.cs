4ab9e70eed7d7a15dfde7228d28e52adc5877f84
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-03-12 09:53:27 -0300

    add logic for message

--- FuegoDeQuasar.Core/Operations/MessageOperation.cs
+++ FuegoDeQuasar.Core/Operations/MessageOperation.cs
@@ -9,10 +9,17 @@ namespace FuegoDeQuasar.Core.Operations
         public string GetMessage(string[] messages)
         {
             string result = string.Empty;
+            
             for (int i = 0; i < messages.Length; i++)
             {
-                result += messages[i] + " ";
+                if ( !string.IsNullOrEmpty(messages[i])) result += messages[i] + " ";
             }
+            result = result.Substring(0, result.Length - 1);
+            //
+            var resultLength = result.Split(" ");
+            int percentage = ((resultLength.Length * 100) / messages.Length);
+            // Si la cantidad de letras obtenidas, y vinieron muchos espacios en el mensaje original, doy ell mensaje como invalido.
+            if (percentage <= 50) result = string.Empty;
             return result;
 
         }
